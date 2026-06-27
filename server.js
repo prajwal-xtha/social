8005f11d08f4280a7a54fe65948b7b81cee68b67
prajwal-xtha <prajwalshrestha794@gmail.com>
2026-06-27 18:31:50 +0545

    first commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express=require('express')
 const cors = require("cors");
 // const welcome=require('./routes/welcome')
 const app=express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 const connectdb=require('./database/db')
 const routes=require('./routes/routes')
 
